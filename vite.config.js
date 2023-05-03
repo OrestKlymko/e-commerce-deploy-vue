100f6150091c857d8c61da704ce6fa7cc9a21bc9
Orest Klymko <116631168+OrestKlymko@users.noreply.github.com>
2023-05-03 20:50:45 +0200

    update

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,9 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base:'https://e-commerce-deploy-vue.vercel.app',
   server: {
     proxy: {
-      '/api': 'https://e-com-website-on-vue-3.herokuapp.com',
+      '/api/': 'https://e-com-website-on-vue-3.herokuapp.com',
     },
   },
 
