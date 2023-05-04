c3506996bfaca50c334447c8ee44f38b4fbbcd18
Orest Klymko <116631168+OrestKlymko@users.noreply.github.com>
2023-05-04 08:37:46 +0200

    update

--- vite.config.js
+++ vite.config.js
@@ -4,12 +4,6 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  server: {
-    proxy: {
-      '/api': 'https://e-com-website-on-vue-3.herokuapp.com',
-    },
-  },
-
 })
 
 
